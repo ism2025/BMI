e61fc917c2bca0515c4a2a2b366b89c328b85461
ism2025 <abo2013hamza@gmail.com>
2018-07-17 04:22:11 +0300

    add facebook api

--- app/src/main/java/com/example/ismailamassi/bmi/MainActivity.java
+++ app/src/main/java/com/example/ismailamassi/bmi/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.ismailamassi.bmi;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
